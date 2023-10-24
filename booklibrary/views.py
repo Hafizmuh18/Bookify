b447c6651d68cb03f3df4cc91b2d5ec8a622e89e
DarilRaf <darilmrafansyah@gmail.com>
2023-10-24 20:01:10 +0700

    Base Project

--- booklibrary/views.py
+++ booklibrary/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import redirect
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from books.models import Books
+
 # Create your views here.
 def show_library(request):
-    pass
+    books = Books.objects.all()
+
+    context = {
+        'books' : books,
+    }
+
+    return render(request, 'library.html', context)
