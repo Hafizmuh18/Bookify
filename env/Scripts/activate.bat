d32b623000227e147d55bab8e4169d6f02be37ce
Hilmi Atha Putra <108039453+hilmiatha@users.noreply.github.com>
2023-10-29 10:06:07 +0700

    added book community module

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Daril\OneDrive\Desktop\Bookify\env
+set VIRTUAL_ENV=D:\Education\College - Information systems\SEMESTER 3\PBP\TK\web clone\clone 1\Bookify\env
 
 if not defined PROMPT set PROMPT=$P$G
 
